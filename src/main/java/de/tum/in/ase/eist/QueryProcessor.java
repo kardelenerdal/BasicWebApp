9a405f19e013360b520b0a823cd14ea28fdd4875
kardelenerdal <kardelenerdal19@gmail.com>
2022-07-14 13:25:01 +0200

    numbers

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -1,19 +1,31 @@
 package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
+
 // kardelen
 @Service
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Kardelen";
-        } else { // TODO extend the programm here
+            return "Kardelen";
+        } else if (query.contains("largest")) {
+            String[] parts = query.split(" ");
+            int num1 = Integer.parseInt(parts[parts.length - 1]);
+            String num2 = parts[parts.length - 2];
+            int num2int = Integer.parseInt(num2.substring(0, num2.length() - 1));
+            return Integer.toString(Math.max(num1, num2int));
+        } else if (query.contains("plus")) {
+            String[] parts = query.split(" ");
+            int num1 = Integer.parseInt(parts[2]);
+            int num2 = Integer.parseInt(parts[4]);
+            return Integer.toString(num1 + num2);
+        } else {
             return "";
         }
     }
