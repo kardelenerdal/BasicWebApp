1e49565b90bfdf38f338acf0756df7135fa16aae
kardelenerdal <kardelenerdal19@gmail.com>
2022-07-14 13:44:39 +0200

    numbers

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,6 +6,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class QueryProcessor {
 
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public String process(String query) {
         query = query.toLowerCase();
         if (query.contains("shakespeare")) {
@@ -33,6 +45,45 @@ public class QueryProcessor {
             int num1 = Integer.parseInt(parts[2]);
             int num2 = Integer.parseInt(parts[4]);
             return Integer.toString(num1 + num2);
+        } else if (query.contains("multiplied")) {
+            String[] parts = query.split(" ");
+            int num1 = Integer.parseInt(parts[2]);
+            int num2 = Integer.parseInt(parts[5]);
+            return Integer.toString(num1 * num2);
+        } else if (query.contains("square") && query.contains("cube")) {
+            String[] parts = query.split(" ");
+            int res = 0;
+            for (String x : parts) {
+                if (x.charAt(0) <= '9' && x.charAt(0) >= '0') {
+                    if (x.charAt(x.length() - 1) == ',') {
+                        x = x.substring(0, x.length() - 1);
+                    }
+                    int num = Integer.parseInt(x);
+                    double sqrt = Math.sqrt(num);
+                    double cubeRoot = Math.cbrt(num);
+                    if (sqrt - Math.floor(sqrt) == 0 && Math.round(cubeRoot) == cubeRoot) {
+                        res = num;
+                        break;
+                    }
+                }
+            }
+            return Integer.toString(res);
+        } else if (query.contains("primes")) {
+            String[] parts = query.split(" ");
+            int res = 0;
+            for (String x : parts) {
+                if (x.charAt(0) <= '9' && x.charAt(0) >= '0') {
+                    if (x.charAt(x.length() - 1) == ',') {
+                        x = x.substring(0, x.length() - 1);
+                    }
+                    if (isPrime(Integer.parseInt(x))) {
+                        res = Integer.parseInt(x);
+                    }
+                }
+            }
+            return Integer.toString(res);
+        } else if (query.contains("eiffel")) {
+            return "Paris";
         } else {
             return "";
         }
